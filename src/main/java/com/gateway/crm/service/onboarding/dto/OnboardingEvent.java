103ecfafb443efe7e3bb13a6aae515b1def9bac7
Manish Talwar <manish.talwar@nexxo.com>
2023-08-18 20:56:10 +0530

    adding Sync route in project

--- src/main/java/com/gateway/crm/service/onboarding/dto/OnboardingEvent.java
+++ src/main/java/com/gateway/crm/service/onboarding/dto/OnboardingEvent.java
@@ -5,13 +5,12 @@ import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
 @Getter
-public class OnboardingEvent extends ApplicationEvent {
+public class OnboardingEvent {
     private JsonNode oldEvent;
     private JsonNode newEvent;
     private String actionType;
 
     public OnboardingEvent(Object oldEvent, Object newEvent, String actionType) {
-        super(oldEvent);
         this.oldEvent = (JsonNode) oldEvent;
         this.newEvent = (JsonNode) newEvent;
         this.actionType = actionType;
