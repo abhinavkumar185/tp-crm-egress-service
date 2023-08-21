2f562523d3c90e8d14abbb8171eb015291c9e8c3
Manish Talwar <manish.talwar@nexxo.com>
2023-08-21 19:19:24 +0530

    adding Sync route in project

--- src/main/java/com/gateway/crm/service/OnboardingGateway.java
+++ src/main/java/com/gateway/crm/service/OnboardingGateway.java
@@ -33,7 +33,7 @@ public class OnboardingGateway {
             log.error("PLAIN PAYLOAD : " +requestData.getPayload() + " : ingressEndpoint URL : " + env.getProperty("ingressEndpoint"));
             String url = env.getProperty("ingressEndpoint")+requestData.getEndPoint();
             //String url = "http://10.0.30.166:9042"+requestData.getEndPoint();
-            HttpEntity<String> entity = new HttpEntity<>(encrypted, requestData.getHeader());
+            HttpEntity<String> entity = new HttpEntity<>(requestData.getHeader());
             log.error("URL : "+url + " : method : " + method + " : entity : " + entity);
             result = restTemplate
                     .exchange(url,method, entity, String.class );
